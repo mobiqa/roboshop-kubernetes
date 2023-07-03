ad143e61303559ad9d6ed2e458f52088bc526e3b
venkat <venkat.mobiqa@gmail.com>
2023-07-03 16:06:59 +0530

    updating ./roboshop-kubernetes

--- parameter-pull/run.sh
+++ parameter-pull/run.sh
@@ -1,14 +1,14 @@
-#set -e
-#
-#aws ssm describe-parameters --region us-east-1 --query "Parameters[*].Name" | xargs -n1  |sed -e 's/,//' | grep ${ENV}.${COMPONENT} >/parameter-store/names
-#
-#rm -f /parameter-store/params
-#
-#for param in `cat /parameter-store/names`; do
-#  VALUE=$(aws ssm get-parameters --region us-east-1 --names $param  --with-decryption --query Parameters[0].Value | sed 's/"//g'  )
-#  KEY=$(echo $param | awk -F . '{print $NF}')
-#  echo export $KEY=\"$VALUE\" >>/parameter-store/params
-#  cat /parameter-store/params
-#done
+set -e
 
-aws ssm describe-parameters --region us-east-1 --query "Parameters[*].Name" | xargs -n1  |sed -e 's/,//' | grep ${ENV}.${COMPONENT}
+aws ssm describe-parameters --region us-east-1 --query "Parameters[*].Name" | xargs -n1  |sed -e 's/,//' | grep ${ENV}.${COMPONENT} >/parameter-store/names
+
+rm -f /parameter-store/params
+
+for param in `cat /parameter-store/names`; do
+  VALUE=$(aws ssm get-parameters --region us-east-1 --names $param  --with-decryption --query Parameters[0].Value | sed 's/"//g'  )
+  KEY=$(echo $param | awk -F . '{print $NF}')
+  echo export $KEY=\"$VALUE\" >>/parameter-store/params
+  cat /parameter-store/params
+done
+
+#aws ssm describe-parameters --region us-east-1 --query "Parameters[*].Name" | xargs -n1  |sed -e 's/,//' | grep ${ENV}.${COMPONENT}
