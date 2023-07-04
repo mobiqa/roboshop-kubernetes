a8df21b293de3eac513543118b02375fe211a62d
venkat <venkat.mobiqa@gmail.com>
2023-07-04 06:15:55 +0530

    updating ./roboshop-kubernetes

--- parameter-pull/run.sh
+++ parameter-pull/run.sh
@@ -2,7 +2,7 @@ set -e
 
 aws ssm describe-parameters --region us-east-1 --query "Parameters[*].Name" | xargs -n1  |sed -e 's/,//' | grep ${ENV}.${COMPONENT} >/parameter-store/names
 
-#rm -f /parameter-store/params
+rm -f /parameter-store/params
 
 for param in `cat /parameter-store/names`; do
   VALUE=$(aws ssm get-parameters --region us-east-1 --names $param  --with-decryption --query Parameters[0].Value | sed 's/"//g'  )
